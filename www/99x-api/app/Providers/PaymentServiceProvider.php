5469d0ef2e171c61dda4c597ff72e98085ce9338
vssuzuki2 <vssuzuki2@gmail.com>
2026-02-13 16:04:05 -0300

    Excercise 1 - Fixing PaymentGateway path in PaymentServiceProvider and includeind enviroment variable for config services.stripe.secret

--- www/99x-api/app/Providers/PaymentServiceProvider.php
+++ www/99x-api/app/Providers/PaymentServiceProvider.php
@@ -7,7 +7,7 @@ class PaymentServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(\App\Services\PaymentGateway::class, function ($app) {
+        $this->app->bind(\App\Contracts\PaymentGateway::class, function ($app) {
             $secret = config('services.stripe.secret');
             return new StripePaymentGateway($secret);
         });
